8c7c7c16b3dbd08894cec5defed2154ab4f2157a
ssucode <ssuya79@gmail.com>
2022-07-02 23:08:59 +0900

    AWS Amplify Integration And Deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import { Input, List} from 'semantic-ui-react';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+import * as queries from './graphql/queries';
+import * as mutation from './graphql/mutations';
+
+Amplify.configure(awsconfig);
+
+const NewTodo = () => {
+  const [name, setName] = useState('')
+
+  const handleSubmit = async(e) => {
+    await API.graphql(graphqlOperation(mutation.createTodo, {input:
+    {name}
+    }));
+    window.location.reload();
+  }
+
+  return (
+    <Input
+      placeholder='new todo'
+      action={{
+        content: 'Create',
+        onClick: handleSubmit
+      }}
+      onChange={(e) => setName(e.target.value)}/>
+  )
+}
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  
+  useEffect(() => {
+    async function fetchData() {
+      const result = await API.graphql(graphqlOperation(queries.listTodos));
+      setTodos(result.data.listTodos.items);
+    }
+    fetchData();
+  },[])
+
+  const todoItems = () => {
+    return todos
+      .map((todo) => <List.Item key={todo.id}>
+        {todo.name}
+      </List.Item>)
+  }
+
+  return (
+    <List>
+      {todoItems()}
+    </List>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' component={NewTodo}/>
+        <Route path='/' component={TodoList}/>
+      </Routes>
     </div>
   );
 }
